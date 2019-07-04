84c6ce4cabb7cb7750343be4da7f16535a9f1606
Vanya Levchenko <ivanlevchenko56@gmail.com>
2019-07-03 23:13:35 -0600

    Add customer and find customer function added.

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connect('mongodb://localhost:27017/customer-cli', {
     useMongoClient: true
 });
+
+// Import model
+const Customer = require('./models/customer');
+
+// Add customer
+const addCustomer = (customer) => {
+    Customer.create(customer).then(customer => {
+        console.info('New Customer Added');
+        db.close();
+    });
+}
+// Find customer
+const findCustomer = (name) => {
+    // Make case insensitive
+    const search = new RegExp(name, 'i');
+    Customer.find({$or: [{firstname: search},{lastname: search}]})
+    .then(customer => {
+        console.info(customer);
+        console.info(`${customer.length} matches`);
+    });
+}
+
+// Export all methoded
+module.exports = {
+    addCustomer,
+    findCustomer
+}
